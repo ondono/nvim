37ce68bb789122318ebdc5f5f849aba5d7c3ae34
Xavi <xavi@ondono.com>
2025-10-04 17:47:42 +0200

    Fixing treesitter small issues by removing unused languages

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = "all",
+    ensure_installed = { "c", "rust", "go", "python", "css", "dockerfile", "git_config","gitignore","devicetree", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "make", "matlab", "regex", "sql", "typescript", "zig"  },
     sync_install = false,
 
     highlight = {
