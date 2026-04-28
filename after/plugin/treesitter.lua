00d0631e836385f2b02c6b8319478b612d4ac549
Xavi Ondono <xavi@ondono.com>
2026-04-28 16:14:00 +0200

    Small fix to treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,30 +1,16 @@
---require'nvim-treesitter.configs'.setup {
---    ensure_installed = { "c", "rust", "go", "python", "css", "dockerfile", "git_config","gitignore","devicetree", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "make", "matlab", "regex", "sql", "typescript", "zig"  },
---    sync_install = false,
---
---    highlight = {
---        enable = true,
---        additional_vim_regex_highlighting = false,
---    },
---}
---
+require("nvim-treesitter.configs").setup({
+    ensure_installed = {
+        "c", "rust", "go", "python", "css", "dockerfile",
+        "git_config", "gitignore", "devicetree",
+        "lua", "vim", "vimdoc", "query",
+        "markdown", "markdown_inline",
+        "make", "matlab", "regex", "sql",
+        "typescript", "zig",
+    },
+    sync_install = false,
 
--- Enable Tree-sitter highlighting
---
-require("nvim-treesitter").install({
-  "c", "rust", "go", "python", "css", "dockerfile",
-  "git_config", "gitignore", "devicetree",
-  "lua", "vim", "vimdoc", "query",
-  "markdown", "markdown_inline",
-  "make", "matlab", "regex", "sql",
-  "typescript", "zig"
-})
-vim.api.nvim_create_autocmd("FileType", {
-  callback = function(args)
-    -- disable regex highlighting
-    vim.bo[args.buf].syntax = "off"
-
-    -- start treesitter
-    pcall(vim.treesitter.start, args.buf)
-  end,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
 })
